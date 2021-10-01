f2280ab6121a21007ec384f266c480a487913867
Gabriellfsj <gabriellfsj@gmail.com>
2021-09-30 22:04:16 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato frajola = new Gato();
+        System.out.println(frajola);
+
+
+    }
+
+}
+
+
